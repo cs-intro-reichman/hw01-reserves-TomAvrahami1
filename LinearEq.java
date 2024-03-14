d4acb0ebb63adb8a94cc84062772090b52102522
TomAvrahami1 <tom.avrahami@runi.ac.il>
2024-03-14 08:36:53 +0200

    last

--- LinearEq.java
+++ LinearEq.java
@@ -9,7 +9,7 @@ public class LinearEq {
 		double a = Double.parseDouble(args[0]),
 		       b = Double.parseDouble(args[1]),
 		       c = Double.parseDouble(args[2]);
-				System.out.println( a + "*"+" x "+"+ "+ b +" = ");
+				System.out.println( a + " *"+" x "+"+ "+ b +" = "+ c);
 			   System.out.println("x = " + ((c - b) / a));
 	}
 }
